cc11a0ef86ae74346d948ff0d1c1661ee62d3cfc
yfann <yfann2012@163.com>
2017-06-10 19:51:53 +0800

    update

--- doubanSpider/settings.py
+++ doubanSpider/settings.py
@@ -89,5 +89,11 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-
+#
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36' 
+
+ITEM_PIPELINES = ['stack.pipelines.DoubanspiderPipeline']
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "scrapyDB"
+MONGODB_COLLECTION = "doubanUsers"
