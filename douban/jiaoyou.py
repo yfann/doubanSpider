868b6310119f08e277cfc8788decde15b1dcd479
Fan <fan.yu@philips.com>
2019-06-12 23:00:42 +0800

    update

---
+++ douban/jiaoyou.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+import time
+
+
+driver=webdriver.Chrome()               
+driver.get("https://www.douban.com/gallery/topic/51644/?sort=new&from=gallery_following_post&guest_only=0") 
